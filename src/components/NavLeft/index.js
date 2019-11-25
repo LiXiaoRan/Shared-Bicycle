663fdd27189a57409260664f7040dc2bac5d3568
997843911@qq.com <997843911@qq.com>
2019-11-25 16:46:47 +0800

    左侧菜单栏渲染基本实现

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,27 +1,53 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import menuConfig from '../../config/menuConfig';
-import { Menu, Icon} from "antd";
+import { Menu, Icon } from "antd";
+import "./index.less";
+
 const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
 
 class NavLeft extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            menuTreeNode: {}
+
+        }
+        this.renderMenu = this.renderMenu.bind(this)
+    }
+
+    componentWillMount() {
+        let menuTreeNode = this.renderMenu(menuConfig);
+        this.setState({
+            menuTreeNode
+        })
+    }
+
+    /**菜单渲染 */
+    renderMenu(data) {
+        
+        return data.map((item) => {
+            if (item.children) {
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        {this.renderMenu(item.children)}
+                    </SubMenu>
+                );
+            }
+            return <Menu.Item title={item.title} key={item.key}>{item.title}</Menu.Item>
+        });
+    }
+
     render() {
         return (
             <div>
                 <div className="logo">
                     {/* 这里写成 assets 是因为在外面访问不到public，这是正常写法*/}
                     <img src="assets/logo192.png" alt={'logo'}></img>
-                    <h1>🚴‍♀️管理系统</h1>
+                    <h1>🚴‍♀️系统</h1>
                 </div>
-                <Menu>
-                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                            <Menu.Item key="1">Option 1</Menu.Item>
-                            <Menu.Item key="2">Option 2</Menu.Item>
-                            <Menu.Item key="3">Option 3</Menu.Item>
-                            <Menu.Item key="4">Option 4</Menu.Item>   
-                    </SubMenu>
+                <Menu theme="dark">
+                    {this.state.menuTreeNode}
                 </Menu>
             </div>
         );
