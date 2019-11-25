376f6a2c511761943255eee659e3114ae9100eb0
997843911@qq.com <997843911@qq.com>
2019-11-25 11:17:19 +0800

    导入antD，并且写了一个button案例和svg案例

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './inde.less'
-import App from './App';
+// import App from './App';
+import PreTest from './PreTest'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App >
-    <div className="mydiv">mydiv</div>
-</App>, document.getElementById('root'));
+ReactDOM.render(<PreTest />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
