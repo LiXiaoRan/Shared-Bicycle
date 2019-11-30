8b1a137c154c4b9037f5994d134b3d21ddbf1030
997843911@qq.com <997843911@qq.com>
2019-11-30 13:51:13 +0800

    使用getFieldDecorator完成了双向数据绑定的封装

--- src/pages/form/login.js
+++ src/pages/form/login.js
@@ -2,20 +2,47 @@ import React, { Component } from "react";
 import { Card, Form, Input, Button } from "antd";
 
 const Item = Form.Item;
-export default class login extends Component {
+class login extends Component {
     render() {
+        // antd封装的form,用来做表单功能的封装.相当于双向数据绑定
+        const { getFieldDecorator } = this.props.form;
         return (
             <div>
                 <Card title="登陆inline表单">
                     <Form layout="inline">
                         <Item>
-                            <Input placeholder="请输入用户名" />
+                            <Input placeholder="请输入用户名" />                            
                         </Item>
                         <Item>
                             <Input placeholder="请输入密码" />
                         </Item>
+
                         <Item>
                             <Button type="primary">登陆</Button>
+                        </Item>
+                    </Form>
+                </Card>
+                <Card
+                    title="登陆水平(horizontal)表单"
+                    style={{ marginTop: 10 }}
+                >
+                    <Form layout="horizontal" style={{ width: 300 }}>
+                        <Item>
+                            {getFieldDecorator("userName", {
+                                initialValue: "lee",
+                                rules: []
+                            })(<Input placeholder="请输入用户名" />)}
+                        </Item>
+                        <Item>
+                            {getFieldDecorator("passWd", {
+                                initialValue: "123456",
+                                rules: []
+                            })(<Input placeholder="请输入密码" />)}
+                        </Item>
+                        <Item>
+                            <Button type="primary" style={{ width: 300 }}>
+                                登陆
+                            </Button>
                         </Item>
                     </Form>
                 </Card>
@@ -23,3 +50,6 @@ export default class login extends Component {
         );
     }
 }
+
+// const FormLogin = Form.create()(login);
+export default Form.create()(login);
