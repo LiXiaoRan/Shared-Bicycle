b7463bd68c391ebc8d856becbc095c13f8af1d6e
997843911@qq.com <997843911@qq.com>
2019-11-30 14:30:30 +0800

    增加了登陆页面的校验

--- src/pages/form/login.js
+++ src/pages/form/login.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
-import { Card, Form, Input, Button } from "antd";
+import { Card, Form, Input, Button, message, Icon, Checkbox } from "antd";
+import { checkServerIdentity } from "tls";
 
 const Item = Form.Item;
 class login extends Component {
+    handleSubmit() {
+        let userInfo = this.props.form.getFieldsValue(); //可以获取到所有表单的值
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                //验证所有的表单状态，如果发生一个错误，err就不为空
+                message.success(
+                    `${userInfo.userName} 恭喜你，通过验证，当前密码为 ：${userInfo.passWd}`
+                );
+            }
+        });
+    }
+
     render() {
         // antd封装的form,用来做表单功能的封装.相当于双向数据绑定
         const { getFieldDecorator } = this.props.form;
@@ -11,7 +24,7 @@ class login extends Component {
                 <Card title="登陆inline表单">
                     <Form layout="inline">
                         <Item>
-                            <Input placeholder="请输入用户名" />                            
+                            <Input placeholder="请输入用户名" />
                         </Item>
                         <Item>
                             <Input placeholder="请输入密码" />
@@ -29,18 +42,58 @@ class login extends Component {
                     <Form layout="horizontal" style={{ width: 300 }}>
                         <Item>
                             {getFieldDecorator("userName", {
-                                initialValue: "lee",
-                                rules: []
-                            })(<Input placeholder="请输入用户名" />)}
+                                initialValue: "lee Ran",
+                                rules: [
+                                    {
+                                        required: true,
+                                        message: "用户名不能为空"
+                                    },
+                                    {
+                                        min: 5,
+                                        max: 10, //最小长度是5，最大长度是10
+                                        message: "长度不在范围内"
+                                    },
+                                    {
+                                        pattern: /^\w+$/g,
+                                        message: "用户名必须是字母或者数字"
+                                    }
+                                ]
+                            })(
+                                <Input
+                                    prefix={<Icon type="user" />}
+                                    placeholder="请输入用户名"
+                                />
+                            )}
                         </Item>
                         <Item>
                             {getFieldDecorator("passWd", {
                                 initialValue: "123456",
                                 rules: []
-                            })(<Input placeholder="请输入密码" />)}
+                            })(
+                                <Input
+                                    prefix={<Icon type="lock" />}
+                                    placeholder="请输入密码"
+                                />
+                            )}
                         </Item>
+
                         <Item>
-                            <Button type="primary" style={{ width: 300 }}>
+                            {getFieldDecorator("remberPWD", {
+                                valuePropName: "checked",
+                                initialValue: true,
+                                rules: []
+                            })(<Checkbox>记住密码</Checkbox>)}
+                            <a href="#" style={{ float: "right" }}>
+                                忘记密码
+                            </a>
+                        </Item>
+
+                        <Item>
+                            <Button
+                                type="primary"
+                                onClick={this.handleSubmit.bind(this)}
+                                style={{ width: 300 }}
+                            >
                                 登陆
                             </Button>
                         </Item>
